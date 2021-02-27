c0883ceffcf7afaaa6944f9eabff181de3a2f16e
cwork <colbycwork@gmail.com>
2021-02-26 19:40:17 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      light: '#F3F3F3',
+      dark: '#272838',
+      green: '#CBFF8C',
+      blue: '#0091AD',
+      red: '#D81159'
+    },
+    fontFamily: {
+      display: '"Fira Code", monospace',
+      content: '"Source Sans Pro", sans-serif'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
